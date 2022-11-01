b4698d72df20a91c0f15ca6d3e5e3d2ccc352cc9
Fakhry-alta <fakhry.ikhsan.firdaus@gmail.com>
2022-11-01 10:00:30 +0700

    update unit test resources, add testify

--- 4-intro-unit-test/calculator/library/calculate.go
+++ 4-intro-unit-test/calculator/library/calculate.go
@@ -16,3 +16,19 @@ func Pembagian(bil1, bil2 int) int {
 	hasil := bil1 / bil2
 	return hasil
 }
+
+func KonversiNilai(nilai int) string {
+	var result string
+	if nilai > 85 && nilai <= 100 {
+		result = "A"
+	} else if nilai >= 65 && nilai <= 85 {
+		result = "B+"
+	} else if nilai >= 50 && nilai < 65 {
+		result = "B"
+	} else if nilai >= 0 && nilai < 50 {
+		result = "D"
+	} else {
+		result = "format nilai tidak sesuai"
+	}
+	return result
+}
--- 4-intro-unit-test/calculator/library/calculate_test.go
+++ 4-intro-unit-test/calculator/library/calculate_test.go
@@ -1,6 +1,10 @@
 package library
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestPenambahan(t *testing.T) {
 	t.Run("Test Fungsi Penambahan dengan 2 bilangan positif", func(t *testing.T) {
@@ -23,5 +27,59 @@ func TestPenambahan(t *testing.T) {
 			t.Error("hasil penambahan tdak sesuai. actual=", actual)
 		}
 
+	})
+
+	t.Run("Test Fungsi Penambahan dengan 1 bilangan negatif dan 1 bilangan positif", func(t *testing.T) {
+		a := -15
+		b := 20
+		// expected := 5
+		actual := Penambahan(a, b)
+		assert.Equal(t, 5, actual, "hasil penambahan tidak sesuai")
+
+	})
+
+}
+
+func TestKurang(t *testing.T) {
+	t.Run("Test Fungsi Pengurangan dengan 2 bilangan positif", func(t *testing.T) {
+		a := 30
+		b := 20
+		expected := 10
+		actual := Pengurangan(a, b)
+		assert.Equal(t, expected, actual, "hasil pengurangan tidak sesuai")
+	})
+
+	t.Run("Test Fungsi Pengurangan dengan 2 bilangan negatif", func(t *testing.T) {
+		a := -30
+		b := -20
+		expected := -10
+		actual := Pengurangan(a, b)
+		assert.Equal(t, expected, actual, "hasil pengurangan tidak sesuai")
+	})
+}
+
+func TestPembagian(t *testing.T) {
+	t.Run("Test Fungsi Pembagian dengan 2 bilangan positif", func(t *testing.T) {
+		a := 10
+		b := 2
+		expected := 5
+		actual := Pembagian(a, b)
+		assert.Equal(t, expected, actual, "hasil pembagian tidak sesuai")
+	})
+}
+
+func TestKonversiNilai(t *testing.T) {
+	t.Run("Test Fungsi Konversi Nilai dengan nilai = 90", func(t *testing.T) {
+		data := 90
+		expected := "A"
+		actual := KonversiNilai(data)
+		assert.Equal(t, expected, actual, "hasil konversi tidak sesuai")
+	})
+
+	t.Run("Test Fungsi Konversi Nilai dengan nilai = 80", func(t *testing.T) {
+		data := 80
+		expected := "B+"
+		actual := KonversiNilai(data)
+		assert.Equal(t, expected, actual, "hasil konversi tidak sesuai")
 	})
 }
